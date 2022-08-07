b505de64ced1491f1e3f07584d001b0e95420edf
SaggyOn <sagarrasal333@gmail.com>
2022-08-07 19:26:15 +0530

    canges

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,449 @@
+<template>
+  <div>
+    <!--Carousel Wrapper-->
+    <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+      <!--Indicators-->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-2" data-slide-to="1"></li>
+        <li data-target="#carousel-example-2" data-slide-to="2"></li>
+      </ol>
+      <!--/.Indicators-->
+      <!--Slides-->
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+          <div class="view">
+            <img class="d-block w-100" src="../../images/home-bg-1.jpg"
+              alt="First slide">
+            <div class="mask rgba-black-light"></div>
+          </div>
+          <div class="carousel-caption">
+            <div class="content">
+                          <span>Explore The</span>
+                          <h3>New World</h3>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit unde ex molestias soluta consequatur saepe aliquam, excepturi delectus consequuntur minus!</p>
+                          <a href="#" class="btn btn-outline-dark">Get Started</a>
+                      </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <!--Mask color-->
+          <div class="view">
+            <img class="d-block w-100" src="../../images/home-bg-2.jpg"
+              alt="Second slide">
+            <div class="mask rgba-black-strong"></div>
+          </div>
+          <div class="carousel-caption-2">
+            <div class="content">
+            <span>Never Stop</span>
+            <h3>Exploring</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit unde ex molestias soluta consequatur saepe aliquam, excepturi delectus consequuntur minus!</p>
+              <a href="#" class="btn btn-outline-dark">Get Started</a>
+              </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <!--Mask color-->
+          <div class="view">
+            <img class="d-block w-100" src="../../images/home-bg-3.jpg"
+              alt="Third slide">
+            <div class="mask rgba-black-slight"></div>
+          </div>
+          <div class="carousel-caption">
+            <div class="content">
+            <span>Never Stop</span>
+            <h3>Exploring</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit unde ex molestias soluta consequatur saepe aliquam, excepturi delectus consequuntur minus!</p>
+              <a href="#" class="btn btn-outline-dark">Get Started</a>
+              </div>
+          </div>
+        </div>
+      </div>
+      <!--/.Slides-->
+      <!--Controls-->
+      <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+      <!--/.Controls-->
+    </div>
+    <!--/.Carousel Wrapper--> 
+
+    <!-- secon dev  -->
+    <div class="container my-3 mb-4">
+      <div class="row ">
+        <span class="my-5">Adventure Idea!</span>
+        <div class="col-md-3">
+          <div class="cards">
+              <img src="../../images/category-1.jpg" class="card-img-top rounded-circle" alt="...">
+            <div class="card-body mt-4">
+              <h5 class="card-title">Bungee Jump</h5>
+              <p class="card-text mt-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-outline-dark">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="cards" >
+              <img src="../../images/category-2.jpg" class="card-img-top rounded-circle" alt="...">
+            <div class="card-body mt-4">
+              <h5 class="card-title">Zip Lines</h5>
+              <p class="card-text mt-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-outline-dark">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="cards" >
+              <img src="../../images/category-3.jpg" class="card-img-top rounded-circle" alt="...">
+            <div class="card-body mt-4">
+              <h5 class="card-title">Canoeing</h5>
+              <p class="card-text mt-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-outline-dark">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="cards" >
+              <img src="../../images/category-4.jpg" class="card-img-top rounded-circle" alt="...">
+            <div class="card-body mt-4">
+              <h5 class="card-title">Kayaking</h5>
+              <p class="card-text mt-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-outline-dark">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- third cont -->
+    <div class="container-fluid about mt-5 pt-2">
+      <div class="row ">
+        <div class="col-md-6 my-5">
+          <img src="../../images/about-img.jpg" alt="" class="img-fluid">
+        </div>
+        <div class="col-md-6 contents m-auto">
+          <h3>Memorable Outdoor Experiences</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque voluptates corrupti natus necessitatibus beatae voluptatibus, deserunt quo soluta minima libero laborum, corporis error esse vitae placeat blanditiis reiciendis vel? Minima.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident dicta doloremque placeat porro, ipsam quia at beatae atque odit iste?</p>
+          <a href="#" class="btn btn-outline-dark">Read More</a>
+        </div>
+      </div>
+    </div>
+    <!-- carosol -->
+
+    <!--/.Carousel Wrapper-->
+    <div class="container">
+      <div class="row">
+        <span class="my-5">Featured Products !</span>
+        <div class="col-md-3 ">
+          <div class="cardp" style="width: 18rem;">
+            <img src="../../images/product-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <span>Card title</span>
+              <h5 class="card-title my-2"> $5.00 - $25.00 </h5>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star-half-stroke"></i>         
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="cardp" style="width: 18rem;">
+            <img src="../../images/product-6.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <span>Card title</span>
+              <h5 class="card-title my-2 "> $5.00 - $25.00 </h5>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star-half-stroke"></i>    
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="cardp" style="width: 18rem;">
+            <img src="../../images/product-5.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <span>Card title</span>
+              <h5 class="card-title my-2"> $5.00 - $25.00 </h5>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star-half-stroke"></i>   
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="cardp" style="width: 18rem;">
+            <img src="../../images/product-2.jpg" class="card-img-top" alt="...">
+            <div class="card-body ">
+              <span>Card title</span>
+              <h5 class="card-title my-2"> $5.00 - $25.00 </h5>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star-half-stroke"></i>   
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
+      <div class="row">
+        <span class="my-5">Adventure Idea!</span>
+        <div class="col-md-4">
+          <div class="cardb" style="width: 24rem;">
+            <img src="../../images/img-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <span>Card title</span>              
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title"> $5.00 - $25.00 </h5>
+              <a href="#" class="btn btn-outline-dark mt-2">Go somewhere</a>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="cardb" style="width: 24rem;">
+            <img src="../../images/img-2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <span>Card title</span>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title"> $5.00 - $25.00 </h5>
+              <a href="#" class="btn btn-outline-dark mt-2">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="cardb" style="width: 24rem;">
+            <img src="../../images/img-3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <span>Card title</span>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title"> $5.00 - $25.00 </h5>
+              <a href="#" class="btn btn-outline-dark mt-2">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+
+
+
+<div class="container">
+<div class="row">
+<div class="col-md-4 col-sm-6">
+<div class="block-text rel zmin">
+<a title="" href="#">Hercules</a>
+<div class="mark">My rating: <span class="rating-input"><span data-value="0" class="glyphicon glyphicon-star"></span><span data-value="1" class="glyphicon glyphicon-star"></span><span data-value="2" class="glyphicon glyphicon-star"></span><span data-value="3" class="glyphicon glyphicon-star"></span><span data-value="4" class="glyphicon glyphicon-star-empty"></span><span data-value="5" class="glyphicon glyphicon-star-empty"></span>  </span></div>
+<p>Never before has there been a good film portrayal of ancient Greece's favourite myth. So why would Hollywood start now? This latest attempt at bringing the son of Zeus to the big screen is brought to us by X-Men: The last Stand director Brett Ratner. If the name of the director wasn't enough to dissuade ...</p>
+<ins class="ab zmin sprite sprite-i-triangle block"></ins>
+</div>
+<div class="person-text rel">
+<img src=""/>
+<a title="" href="#">Anna</a>
+<i>from Glasgow, Scotland</i>
+</div>
+</div>
+</div>
+</div>
+    
+  </div>
+</template>
+
+<style scoped>
+.carousel-caption {
+    position: absolute;
+    right: 1%;
+    bottom: 16rem;
+    left: 54%;
+    padding-top: 4.25rem;
+    padding-bottom: 1.25rem;
+    text-align: center;
+}
+.carousel-caption-2 {
+    position: absolute;
+    right: 20%;
+    bottom: 16rem;
+    left: 20%;
+    padding-top: 4.25rem;
+    padding-bottom: 1.25rem;
+    text-align: center;
+}
+p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    padding: 1rem;
+}
+ .content {
+  width: 25rem;
+}
+
+  span {
+  font-size: 2rem;
+  color: #10221b;
+}
+
+ .content h3 {
+  font-size: 3rem;
+  color: #219150;
+  padding-top: .5rem;
+  text-transform: uppercase;
+}
+
+ .content p {
+  line-height: 2;
+  color: #10221b;
+  font-size: 1.rem;
+  padding: 1rem 0;
+}
+
+
+.cards{
+  width: 18rem;
+  border: none;
+}
+.card-title{
+  font-size: 1.5rem;
+  color: #219150;
+}
+.rounded-circle {
+    border-radius: 50% !important;
+    width: 16rem;
+}
+
+
+
+.about {
+  background: #eee;
+  
+}
+
+.about .image img {
+  
+}
+
+
+
+ .contents h3 {
+  font-size: 3rem;
+  color: #219150;
+}
+
+.contents p {
+  font-size: 1.3rem;
+  color: #10221b;
+  padding: 1rem 0;
+  line-height: 2;
+}
+
+.cardp{
+  border: 2px solid #000;
+  margin-bottom: 2px;
+}
+.cardb{
+  border: 2px solid #000;
+  border-radius: 0.5rem;
+}
+.card-body {
+    margin-bottom: 1rem;
+}
+.cardb img{
+  padding: 1rem;
+    border-radius: 1.5rem;
+    width: 23rem;
+}
+
+
+
+
+.block-text {
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 3px 0 #2c2222;
+    color: #626262;
+    font-size: 14px;
+    margin-top: 27px;
+    padding: 15px 18px;
+}
+.block-text a {
+ color: #7d4702;
+    font-size: 25px;
+    font-weight: bold;
+    line-height: 21px;
+    text-decoration: none;
+    text-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+}
+.mark {
+    padding: 12px 0;background:none;
+}
+.block-text p {
+    color: #585858;
+    font-family: Georgia;
+    font-style: italic;
+    line-height: 20px;
+}
+
+.sprite-i-triangle {
+    background-position: 0 -1298px;
+    height: 44px;
+    width: 50px;
+}
+.block-text ins {
+    bottom: -44px;
+    left: 50%;
+    margin-left: -60px;
+}
+
+
+.block {
+    display: block;
+}
+.zmin {
+    z-index: 1;
+}
+.ab {
+    position: absolute;
+}
+
+.person-text {
+    padding: 10px 0 0;
+    text-align: center;
+    z-index: 2;
+}
+.person-text a {
+    color: #ffcc00;
+    display: block;
+    font-size: 14px;
+    margin-top: 3px;
+    text-decoration: underline;
+}
+.person-text i {
+    color: #fff;
+    font-family: Georgia;
+    font-size: 13px;
+}
+.rel {
+    position: relative;
+}
+.broun-block {
+    background: url("../../images/serv-2.png") repeat scroll center top rgba(0, 0, 0, 0);
+    padding-bottom: 34px;
+}
+</style>
